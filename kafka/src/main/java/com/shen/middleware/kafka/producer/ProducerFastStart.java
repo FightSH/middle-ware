b26d8ba27c92974a4b8970c013719dcb8df822d3
shen <shenhao@iyunbao.com>
2021-12-24 18:42:15 +0800

    easy demo

--- kafka/src/main/java/com/shen/middleware/kafka/producer/ProducerFastStart.java
+++ kafka/src/main/java/com/shen/middleware/kafka/producer/ProducerFastStart.java
@@ -1,6 +1,7 @@
 package com.shen.middleware.kafka.producer;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
@@ -10,13 +11,21 @@ public class ProducerFastStart {
     public static final String brokerList = "localhost:9092";
     public static final String topic = "topic-demo";
 
-    public static void main(String[] args) {
+
+    public static Properties initConfig() {
         Properties properties = new Properties();
-        properties.put("key.serializer",
-                "org.apache.kafka.common.serialization.StringSerializer");
-        properties.put("value.serializer",
-                "org.apache.kafka.common.serialization.StringSerializer");
-        properties.put("bootstrap.servers", brokerList);
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                StringSerializer.class.getName());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                StringSerializer.class.getName());
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, brokerList);
+        properties.put(ProducerConfig.CLIENT_ID_CONFIG, "producer.client.id.demo");
+        return properties;
+
+    }
+
+    public static void main(String[] args) {
+        Properties properties = initConfig();
 
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
 
